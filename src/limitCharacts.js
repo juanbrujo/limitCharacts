8fd38b9e364453bc44b9ab9fb8ba91a015a1ea25
Jorge Epuñan <juanbrujo@gmail.com>
2015-09-02 11:38:12 -0400

    cleaning some stuff

--- src/limitCharacts.js
+++ src/limitCharacts.js
@@ -3,24 +3,24 @@ function limitCharacts(limitField, limitNum) {
   "use strict";
 
   if( limitField.length ) {
-    
+
     [].forEach.call(limitField, function(elem) {
       var dataCount = elem.getAttribute("data-count"),
           countElem = document.querySelector(dataCount);
 
       valueCountElem(countElem,limitNum);
-      
+
       elem.addEventListener("keydown",function(){
         limiter(elem,countElem);
       });
       elem.addEventListener("keyup",function(){
         limiter(elem,countElem);
       });
-      
+
     });
-    
+
   }
-  
+
   function limiter(elem,countElem){
     if (elem.value.length > limitNum) {
       elem.value = elem.value.substring(0, limitNum);
@@ -39,14 +39,3 @@ function limitCharacts(limitField, limitNum) {
   }
 
 }
-
-// module.exports = typeof limitCharacts(limitField, limitNum) === 'function' ? limitCharacts(limitField, limitNum) : limitCharacts(limitField, limitNum);
-
-// if ( typeof module !== "undefined" && module.exports ) {
-//   // export functions for use in Node
-//   module.exports = limitCharacts(limitField, limitNum);
-// } else if ( typeof define !== "undefined" && define.amd ) {
-//   // export function for use in AMD
-//   define([], limitCharacts(limitField, limitNum));
-// }
-
