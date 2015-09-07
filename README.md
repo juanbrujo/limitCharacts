3936cd8a726608fede5913c6db0eac5fed0a1381
Jorge Epuñan <juanbrujo@gmail.com>
2015-09-07 13:49:56 -0300

    better readme

--- README.md
+++ README.md
@@ -1,16 +1,26 @@
-#limitCharacts
+# limitCharacts
 
 > Limits and trim the amount of characters added to any input/textarea tag
 
-###Use
+### Use
 
+#### Web/HTML:
 ```
 var elem = document.querySelectorAll('input'),
     limit = 100;
 limitCharacts(elem,limit);
 ```
 
-###Demo
+#### AMD/CommonJS:
+```
+var limit = require('limitCharacts.js');
+var text = 'Limit this text field';
+var length = 5;
+limit.limitCharacts( text, length );
+```
+
+
+### Demo
 
 [juanbrujo.github.io/limitCharacts](http://juanbrujo.github.io/limitCharacts/)
 
--- test.js
+++ test.js
@@ -3,7 +3,7 @@
  * $ node test.js
  * expected output:
  * - Test 1: Limit
- * - Test 2: ["i-used-to-have-ugly-text-now-i-just-have-1", "solia-tener-texto-feo-ahora-solo-tengo-1", "jai-lhabitude-davoir-un-texte-laid-je-dois-maintenant-juste-1", "fruher-habe-ich-hasslich-text-jetzt-habe-ich-nur-noch-1"]
+ * - Test 2:  TO-DO
  * - Test 3: 👍 fine by
  */
 
@@ -11,26 +11,26 @@ var limit = require('./dist/limitCharacts.min.js');
 
 // Test 1: output 1 string of text
 console.log('\n# Test 1: output 1 string of text, truncate at 5th character:');
-var text1 = "Limit this text field";
-console.log( "Input: " + text1 );
-console.log( "Output: " + limit.limitCharacts( text1, 5 ) );
+var text1 = 'Limit this text field';
+console.log( 'Input: ' + text1 );
+console.log( 'Output: ' + limit.limitCharacts( text1, 5 ) );
 
 // Test 2: output an array of texts
 // TO-DO
 /*
 console.log('\n# Test 2: output an array of texts:');
 var text2 = [
-	"I used to have ugly text. Now I just have 1.",
-	"Solía tener texto feo. Ahora sólo tengo 1.",
-	"J'ai l'habitude d'avoir un texte laid. Je dois maintenant juste 1.",
-	"Früher habe ich hässlich Text. Jetzt habe ich nur noch 1."
+	'I used to have ugly text. Now I just have 1.',
+	'Solía tener texto feo. Ahora sólo tengo 1.',
+	'J'ai l'habitude d'avoir un texte laid. Je dois maintenant juste 1.',
+	'Früher habe ich hässlich Text. Jetzt habe ich nur noch 1.''
 ];
-console.log( "Input: " + text2 );
-console.log( "Output: " + limit.limitCharacts(text2, 10 ) );
+console.log( 'Input: ' + text2 );
+console.log( 'Output: ' + limit.limitCharacts(text2, 10 ) );
 */
 
 // Test 3: input includes emojis
 console.log('\n# Test 3: input includes emojis, truncate at 10th character:');
-var text3 = "👍 fine by me!";
-console.log( "Input: " + text3 );
-console.log( "Output: " + limit.limitCharacts( text3, 10 ) );
+var text3 = '👍 fine by me!';
+console.log( 'Input: ' + text3 );
+console.log( 'Output: ' + limit.limitCharacts( text3, 10 ) );
